cfc2ec4496ef845acaac8d6f4faa5d12bb2f0e79
Ilya Zhuravlev <ichirozcom@gmail.com>
2019-02-07 18:53:35 +0300

    feat(build): add gsconfig for connect geoserver to django

--- web/views.py
+++ web/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from geoserver.catalog import Catalog
+import requests
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {})
+    print("----------------")
+    cat = Catalog("http://geoserver:8080/geoserver/rest/", username="admin", password="geoserver")
+    req = cat.get_layers()
+    # WORKED!!!
+    # req = requests.get("http://geoserver:8080/geoserver/rest/about/version")
+    # session = requests.Session()
+    # req = session.get("http://geoserver:8080/geoserver/rest/about/version")
+    # print(all_layers)
+    testVar = "hello"
+    return render(request, 'index.html', {'testVar' : req[0].name})
