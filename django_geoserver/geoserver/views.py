7d2ce886272fd7903e1fa76a33dca46536521800
Ilya Zhuravlev <ichirozcom@gmail.com>
2019-02-13 15:25:54 +0300

    feat(geoserver): add WFS endpoint and GetFeature Method

--- django_geoserver/geoserver/views.py
+++ django_geoserver/geoserver/views.py
@@ -1,6 +1,49 @@
 from django.http import HttpResponse
+from owslib import wfs
 
 
 def wms_endpoint(request):
     res = "HELLO"
+    return HttpResponse(res, status=200)
+
+
+def wfs_endpoint(request):
+    res = "HELLO"
+    # TODO: check auth & permissions
+    service = wfs.WebFeatureService("http://geoserver:8080/geoserver/wfs", version="1.1.0", username="admin", password="geoserver")
+    if request.method == "GET":
+        # if request without params
+        if request.GET and not request.GET["request"] == "GetCapabilities":
+            # check params
+            if not request.GET["service"] == "WFS":
+                return HttpResponse("bad request", status=404)
+            if not request.GET["version"] == "1.1.0":
+                return HttpResponse("bad request", status=404)
+
+            # if GetFeature
+            if request.GET["request"] == "GetFeature":
+                # get layer
+                if "typename" in request.GET:
+                    typename = request.GET["typename"]
+                    contents = service.contents
+                    # check typename
+                    if typename in list(contents):
+                        res = service.getfeature(typename=typename, outputFormat="json")
+                        return HttpResponse(res.read(), status="200", content_type="application/json")
+                    else:
+                        return HttpResponse("layer not found", status=404)
+                else:
+                    return HttpResponse("typename is required", status=404)
+
+        else:
+            res = service.getcapabilities()
+            return HttpResponse(res.read(), status=200, content_type="text/xml")
+    elif request.method == "POST":
+        res = "POST"
+    else:
+        return HttpResponse("bad request", status=404)
+    # feature = service.getfeature(typename="	test:nyc_buildings", outputFormat="json")
+    # feature = service.getfeature(typename="test:drawing")
+
+    # res = feature.info
     return HttpResponse(res, status=200)
--- django_geoserver/web/views.py
+++ django_geoserver/web/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-# from geoserver.catalog import Catalog
+from geoserver.catalog import Catalog
 from owslib.wms import WebMapService
 from owslib.wfs import WebFeatureService
 
@@ -8,8 +8,8 @@ def index(request):
     #
     # REST_example
     #
-    # cat = Catalog("http://geoserver:8080/geoserver/rest/", username="admin", password="geoserver")
-    # workspace = cat.get_workspace("test")
+    cat = Catalog("http://geoserver:8080/geoserver/rest/", username="admin", password="geoserver")
+    workspace = cat.get_workspace("test")
     # resources = cat.get_resources(workspace=workspace)
     # testVar = resources
     #
@@ -18,9 +18,8 @@ def index(request):
     wms = WebMapService("http://geoserver:8080/geoserver/wms")
     wms_services = list(wms.contents)
     # wfs
-    wfs = WebMapService("http://geoserver:8080/geoserver/wfs", version="1.1.1", username="admin", password="geoserver")
+    wfs = WebFeatureService("http://geoserver:8080/geoserver/wfs", version="1.1.0", username="admin", password="geoserver")
     wfs_services = list(wfs.contents)
     # result
     testVar = "hello"
-    testVar = wfs_services
     return render(request, 'web/index.html', {'testVar' : testVar})
